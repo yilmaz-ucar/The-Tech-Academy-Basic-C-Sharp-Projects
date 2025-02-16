e9056c5bee2a43e85f0ad61d3cc054a9541cf92c
yilmaz-ucar <Yilmaz.ucar@my-aolcc.com>
2025-02-16 20:59:26 +0300

    Method Class Assignment
    Method Class Assignment

---
+++ Method Class Assignment/MathOperations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Method_Class_Assignment
+{
+    public class MathOperations
+    {
+        public void PerformOperation(int num1, int num2)
+        {
+            // The number is raised to the third power.
+            var result = Math.Pow(num1, 2);
+
+            // Display the result of the operation
+            Console.WriteLine($"The result of the number {num1} to the power of 2: {result}");
+
+            // Display the second integer
+            Console.WriteLine($"The second integer is: {num2}");
+        }
+    }
+}
---
+++ Method Class Assignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Method_Class_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the 'MathOperations' class
+            var mathOps = new MathOperations();
+
+            // Call the 'PerformOperation' method, passing in two numbers
+            mathOps.PerformOperation(3, 8);
+
+            // Call the 'PerformOperation' method again, specifying the parameters by name
+            mathOps.PerformOperation(num1: 45, num2: 43);
+
+            // Wait for user input before closing the console window
+            Console.ReadLine();
+        }
+    }
+}
